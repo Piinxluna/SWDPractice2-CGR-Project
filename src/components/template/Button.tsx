a150d213452887472b1d6772f241e027674f434b
Supawit <supawit.ma@gmail.com>
2024-03-20 11:41:16 +0700

    table & textField template update

--- src/components/template/Button.tsx
+++ src/components/template/Button.tsx
@@ -1,19 +1,10 @@
 export default function Button () {
   return(
     <div className="flex flex-col text-center">
-      <h1 className="text-lg font-medium my-2">You Can copy code and paste in to your components</h1>
-      <div className="my-2">
-        <button className='cgr-btn'>Button Primary</button>
-      </div>
-      <div className="my-2">
-        <button className='cgr-btn-outline'>Button Outline</button>
-      </div>
-      <div className="my-2">
-        <button className='cgr-btn-red'>Button Red</button>
-      </div>
-      <div className="my-2">
-        <button className='cgr-btn-outline-gray'>Button Outline Gray</button>
-      </div>
+      <button className='cgr-btn'>Button Primary</button>
+      <button className='cgr-btn-outline'>Button Outline</button>
+      <button className='cgr-btn-red'>Button Red</button>
+      <button className='cgr-btn-outline-gray'>Button Outline Gray</button>
     </div>
   )
 }
