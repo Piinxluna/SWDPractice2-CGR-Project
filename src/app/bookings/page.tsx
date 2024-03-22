b7b5866e153ef411ad6296b781593b6b9d1e05d5
Piinxluna <pingpingkuss@gmail.com>
2024-03-22 12:26:33 +0700

    Component : BookingPanel & Page : Bookings (not finished)

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -50,10 +50,29 @@ export default function Home() {
   ]
 
   return (
-    <main className='bg-cgr-gray-10 p-16 w-screen h-screen'>
-      <div className='mb-5'>Controls</div>
-      <div>
+    <main className='bg-cgr-gray-10 p-16 w-screen min-h-screen px-18'>
+      <div className='flex flex-row flex-wrap justify-start align-items-start space-y-2 mb-8'>
+        <div className='flex flex-row basis-2/5 space-x-3'>
+          <input
+            type='text'
+            className='cgr-search-box placeholder-cgr-dark-green'
+            placeholder='Find something...'
+          />
+          <input
+            type='text'
+            className='cgr-search-box placeholder-cgr-dark-green'
+            placeholder='Find something...'
+          />
+        </div>
+        <div className='basis-0 lg:basis-2/5'></div>
+        <button className='cgr-btn basis-1/5'>Book more</button>
+      </div>
+      <div className='mb-12'>
         <p className='text-3xl font-bold mb-6 z-30'>Incoming Booking</p>
+        <BookingPanel bookings={activeBookings} />
+      </div>
+      <div>
+        <p className='text-3xl font-bold mb-6 z-30'>Achieved Booking</p>
         <BookingPanel bookings={activeBookings} />
       </div>
     </main>
