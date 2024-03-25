87f1e1136e4463f4f93faea25ef031483dbc8220
Wuaimi <141891526+Wuaimi@users.noreply.github.com>
2024-03-26 04:04:20 +0700

    Campground create update

--- src/app/campgrounds/create/page.tsx
+++ src/app/campgrounds/create/page.tsx
@@ -1,167 +1,223 @@
 'use client'
 
 import { useState } from 'react'
-import { MenuItem, Select, TextField } from '@mui/material'
+import { Checkbox, FormControlLabel, MenuItem, Select, TextField } from '@mui/material'
 
 import Card from '@/components/basic/card/Card'
+import CGRCheckBox from '@/components/template/CheckBox'
 
 export default function Component() {
 
 
       const mockCampground = {
         name: 'Campground1',  
-        zone : 'A',
-        sitenumber : '1',
-        preferredname : 'Cg1',
-        amount : '4',
-        tentsizew : '5',
-        tentsizel : '6',
-        date : '3/25/2024'
+        tel : '02-345-6789',
+        address : {
+            houseNo : "123",
+            lane : "222",
+            road : "BanThudThond",
+            subdistrict : "RongMueng",
+            district : "Pathumwan",
+            province : "Bangkok" ,
+            postalcode : "12345"
+        },
+        website : "www.google.com",
       }
     
-      const [cgName,setCgName] = useState(mockCampground.name)
-      const [zone,setZone] = useState(mockCampground.zone)
-      const [siteNumber,setSiteNumber] = useState(mockCampground.sitenumber)
-      const [preferredName,setPreferredName] = useState(mockCampground.preferredname)
-      const [amount,setAmount] = useState(mockCampground.amount)
-      const [tentSizeW,setTentSizeW] = useState(mockCampground.tentsizew)
-      const [tentSizeL,setTentSizeL] = useState(mockCampground.tentsizel)
-      const [date, setDate] = useState (mockCampground.date)
+      const [name,setName] = useState(mockCampground.name)
+      const [tel,setTel] = useState(mockCampground.tel)
+      const [houseNo,setHouseNo] = useState(mockCampground.address.houseNo)
+      const [lane,setLane] = useState(mockCampground.address.lane)
+      const [road,setRoad] = useState(mockCampground.address.road)
+      const [subdistrict,setSubdistrict] = useState(mockCampground.address.subdistrict)
+      const [district,setDistrict] = useState(mockCampground.address.district)
+      const [province,setProvince] = useState(mockCampground.address.province)
+      const [postalcode,setPostalCode] = useState(mockCampground.address.postalcode)
+      const [website,setWebsite] = useState(mockCampground.website)
+    //   const [tentForRent, setTentForRent] = useState(false);
+    //   const [toilet, setToilet] = useState(false);
+    //   const [electricity, setElectricity] = useState(false);
+    //   const [parking, setParking] = useState(false);
+    //   const [wifi, setWifi] = useState(false);
 
  return(
 
     <main className='bg-white px-4 py-8 sm:px-10 md:px-16 lg:px-36 xl:px-72 2xl:px-96 min-h-screen '>
-    <Card>
         <h1 className='text-4xl font-bold text-cgr-dark-green flex justify-center py-12'>
             Create Booking
         </h1>
-        <div className='px-6'>
-        <div className='text-lg gap-5 mb-8 flex flex-col ml-12'>
-            <div className='flex flex-row mr-6'>
-                <div className='w-1/3'><p className='font-medium text-l '>Campground Name:</p></div>
-                <Select
-                    labelId="CamgroundName"
-                    id="campground-name"
-                    value={cgName}
-                    label="Campground Name"
-                    placeholder="Campground Name"
-                    onChange={(e) => setCgName(e.target.value)}
-                    className='w-full'>
-                    <MenuItem value={'campground1'}>Campground1</MenuItem>
-                    <MenuItem value={'campground2'}>Campground2</MenuItem>
-                    <MenuItem value={'campground3'}>Campground3</MenuItem>
-                </Select>
+        <Card>
+            <div className='px-6'>
+            <div className='text-lg gap-5 mb-8 flex flex-col ml-12'>
+                <div className='flex flex-row items-center mr-6 mt-8'>
+                    <div className='w-2/5'><p className='font-medium'>Campground Name :</p></div>
+                    <TextField
+                        required
+                        id='campgroundName'
+                        label='Campground Name'
+                        variant='outlined'
+                        size='small'
+                        InputProps={{ style: { borderRadius: '10px' } }}
+                        className='w-full'
+                        value={name}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setName(event.target.value)
+                        }}
+                    />
+                </div>
+                <div className='flex flex-row items-center mr-6 '>
+                    <div className='w-2/5'><p className='font-medium'>Telephone :</p></div>
+                    <TextField
+                        required
+                        id='tel'
+                        label='Telephone Number'
+                        variant='outlined'
+                        size='small'
+                        InputProps={{ style: { borderRadius: '10px' } }}
+                        className='w-full'
+                        value={tel}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setTel(event.target.value)
+                        }}
+                    />
+                </div>
+                <div className='flex flex-row items-center mr-6 '>
+                    <div className='w-2/5'><p className='font-medium'>Address :</p></div>
+                    <TextField
+                        required
+                        id='houseno'
+                        label='House Number'
+                        variant='outlined'
+                        size='small'
+                        InputProps={{ style: { borderRadius: '10px' } }}
+                        className='w-1/2 mr-4'
+                        value={houseNo}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setHouseNo(event.target.value)
+                        }}
+                    />
+                    <TextField
+                        required
+                        id='lane'
+                        label='Lane'
+                        variant='outlined'
+                        size='small'
+                        InputProps={{ style: { borderRadius: '10px' } }}
+                        className='w-1/2'
+                        value={lane}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setLane(event.target.value)
+                        }}
+                    />
+                </div>
+                <div className='flex flex-row items-center mr-6 '>
+                    <div className='w-2/5'></div>
+                    <TextField
+                        id='road'
+                        label='Road'
+                        variant='outlined'
+                        size='small'
+                        InputProps={{ style: { borderRadius: '10px' } }}
+                        className='w-full'
+                        value={road}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setRoad(event.target.value)
+                        }}
+                    />
+                </div>
+                <div className='flex flex-row items-center mr-6 '>
+                    <div className='w-2/5'></div>
+                    <TextField
+                        required
+                        id='subdistrict'
+                        label='Subdistrict'
+                        variant='outlined'
+                        size='small'
+                        InputProps={{ style: { borderRadius: '10px' } }}
+                        className='w-1/2 mr-4'
+                        value={subdistrict}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setSubdistrict(event.target.value)
+                        }}
+                    />
+                    <TextField
+                        required
+                        id='district'
+                        label='Dictrict'
+                        variant='outlined'
+                        size='small'
+                        InputProps={{ style: { borderRadius: '10px' } }}
+                        className='w-1/2 '
+                        value={district}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setDistrict(event.target.value)
+                        }}
+                    />
+                </div>
+                <div className='flex flex-row items-center mr-6 '>
+                    <div className='w-2/5'></div>
+                    <TextField
+                        required
+                        id='province'
+                        label='Province'
+                        variant='outlined'
+                        size='small'
+                        InputProps={{ style: { borderRadius: '10px' } }}
+                        className='w-1/2 mr-4'
+                        value={province}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setProvince(event.target.value)
+                        }}
+                    />
+                    <TextField
+                        required
+                        id='postalcode'
+                        label='Postal code'
+                        variant='outlined'
+                        size='small'
+                        InputProps={{ style: { borderRadius: '10px' } }}
+                        className='w-1/2'
+                        value={postalcode}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setPostalCode(event.target.value)
+                        }}
+                    />
+                </div>
+                <div className='flex flex-row items-center mr-6 '>
+                <div className='w-2/5'><p className='font-medium'>Website :</p></div>
+                    <TextField
+                        id='website'
+                        label='Website'
+                        variant='outlined'
+                        size='small'
+                        InputProps={{ style: { borderRadius: '10px' } }}
+                        className='w-full'
+                        value={website}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setWebsite(event.target.value)
+                        }}
+                    />
+                </div>
+            <div className='flex flex-row items-center mr-6 '>
+                <div className='w-1/3'><p className='text-medium'>Facility :</p></div>
+                <div className='flex flex-wrap mt-4'>
+                    <FormControlLabel label="Tent for rent" control={<CGRCheckBox/>}/>
+                    <FormControlLabel label="Toilet" control={<CGRCheckBox/>}/>
+                    <FormControlLabel label="Electricity" control={<CGRCheckBox/>}/>
+                    <FormControlLabel label="Parking" control={<CGRCheckBox/>}/>
+                    <FormControlLabel label="Wifi" control={<CGRCheckBox/>}/>
+                </div>
             </div>
-            <div className='flex flex-row items-center mr-6'>
-                <div className='w-1/3'><p className='font-medium'>Zone:</p></div>
-                <Select
-                    labelId="Zone"
-                    id="zone"
-                    value={zone}
-                    label="Zone"
-                    onChange={(e) => setZone(e.target.value)}
-                    className='w-full'>
-                    <MenuItem value={'zoneA'}>A</MenuItem>
-                    <MenuItem value={'zoneB'}>B</MenuItem>
-                    <MenuItem value={'zoneC'}>C</MenuItem>
-                </Select>
+            <div className="mb-1 flex flex-row">
+                <div className='w-2/5'>Upload Image : </div>
+                <div className="">
+                    <input type="file" id="Campground Image" className='mr-28'/>
+                </div>
             </div>
-            <div className='flex flex-row items-center mr-6'>
-                <div className='w-1/3'><p className='font-medium'>Site Number:</p></div>
-                <Select
-                    labelId="Site Number"
-                    id="sitenumber"
-                    value={siteNumber}
-                    label="Site Number"
-                    onChange={(e) => setSiteNumber(e.target.value)}
-                    className='w-full'>
-                    <MenuItem value={'site1'}>1</MenuItem>
-                    <MenuItem value={'site2'}>2</MenuItem>
-                    <MenuItem value={'site3'}>3</MenuItem>
-                </Select>
             </div>
-            <div className='flex flex-row items-center mr-6'>
-                <div className='w-1/3'><p className='font-medium'>Preferred Name:</p></div>
-                <TextField
-                    required
-                    id='preferredName'
-                    label='Name'
-                    variant='outlined'
-                    size='small'
-                    InputProps={{ style: { borderRadius: '10px' } }}
-                    className='w-full'
-                    value={preferredName}
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    setPreferredName(event.target.value)
-                    }}
-                />
-            </div>
-            <div className='flex flex-row items-center mr-6'>
-                <div className='w-1/3'><p className='font-medium'>Amount:</p></div>
-                <TextField
-                    required
-                    id='amount'
-                    label='Amount'
-                    variant='outlined'
-                    size='small'
-                    InputProps={{ style: { borderRadius: '10px' } }}
-                    className='w-full'
-                    value={amount}
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    setAmount(event.target.value)
-                    }}
-                />
-            </div>
-            <div className='flex flex-row items-center mr-6'>
-                <div className='w-1/3'><p className='font-medium'>Tent Size:</p></div>
-                <TextField
-                    required
-                    id='tentSizeW'
-                    label='Width(meter)'
-                    variant='outlined'
-                    size='small'
-                    InputProps={{ style: { borderRadius: '10px' } }}
-                    className='w-1/2 mr-4'
-                    value={tentSizeW}
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    setTentSizeW(event.target.value)
-                    }}
-                />
-                <TextField
-                    required
-                    id='tentSizeL'
-                    label='Length(meter)'
-                    variant='outlined'
-                    size='small'
-                    InputProps={{ style: { borderRadius: '10px' } }}
-                    className='w-1/2'
-                    value={tentSizeL}
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    setTentSizeL(event.target.value)
-                    }}
-                />
-            </div>
-            <div className='flex flex-row items-center mr-6'>
-                <div className='w-1/3'><p className='font-medium'>Date:</p></div>
-                <TextField
-                    type='date'
-                    id='date'
-                    label='Date'
-                    variant='outlined'
-                    size='small'
-                    InputProps={{ style: { borderRadius: '10px' } }}
-                    className='w-full'
-                    value={date}
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    setDate(event.target.value)
-                    }}
-                />
-            </div>
-        </div>
-        <div className='flex justify-center'>
-            <button className='cgr-btn w-1/2'>Done</button>
-        </div>
-        </div>
-    </Card>
+            <div className='flex justify-center py-5'>
+                <button className='cgr-btn w-1/2'>Create</button>
+            </div></div>
+            </Card>
     </main>
  )}
