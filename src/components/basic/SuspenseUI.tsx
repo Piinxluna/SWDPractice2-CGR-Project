e6244fcd9477eb0e022a1d4de0e9e7cae7df5fb7
Piinxluna <pingpingkuss@gmail.com>
2024-03-26 17:42:53 +0700

    Component : Suspense UI & API : getDistricts, getProvinces, getSubDistricts

--- src/components/basic/SuspenseUI.tsx
+++ src/components/basic/SuspenseUI.tsx
@@ -2,7 +2,7 @@ import { CircularProgress, LinearProgress } from '@mui/material'
 
 export default function SuspenseUI() {
   return (
-    <div className='w-full'>
+    <div className='w-full p-32 text-center text-3xl'>
       Loading in progress... <LinearProgress />
     </div>
   )
